fa4091fb3937b89e8b35a36ad48493a3f5609dd2
Mike Chen <517926804@qq.com>
2018-10-25 18:03:10 +0800

    添加youdao-dictionary快捷键绑定配置

---
+++ layers/mike/keybindings.el
@@ -0,0 +1,14 @@
+;;; keybindings.el --- mike layer keybindings file for Spacemacs.
+;;
+;; Copyright (c) 2018 Mike Chen
+;;
+;; Author: 陈显彬 <517926804@qq.com>
+;; URL: https://github.com/cxb811201/spacemacs-private
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; 绑定youdao-dictionary快捷键
+(define-key global-map (kbd "C-c y") 'youdao-dictionary-search-at-point+)
+(spacemacs/set-leader-keys "oy" 'youdao-dictionary-search-at-point+)
