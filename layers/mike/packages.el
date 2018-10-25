19490396c7b54c302a2f410cf2f3c0aaaf4dce62
Mike Chen <517926804@qq.com>
2018-10-25 17:42:18 +0800

    添加显示农历支持

--- layers/mike/packages.el
+++ layers/mike/packages.el
@@ -14,6 +14,7 @@
 (defconst mike-packages
   '(
     fcitx
+    cal-china-x
     ))
 
 ;; fcitx优化
@@ -24,3 +25,24 @@
   (when (spacemacs/system-is-linux)
     (setq fcitx-use-dbus t))
   )
+
+;; 显示农历
+(defun mike/init-cal-china-x ()
+  (use-package cal-china-x
+    :ensure t
+    :init
+    :config
+    (progn
+      (setq mark-holidays-in-calendar t)
+      (setq cal-china-x-important-holidays cal-china-x-chinese-holidays)
+      (setq cal-china-x-general-holidays '(
+                                           (holiday-lunar 1 15 "元宵节")
+                                           (holiday-lunar 7 7 "七夕节")
+                                           (holiday-lunar 9 9 "重阳节")
+                                           ))
+      (setq calendar-holidays
+            (append cal-china-x-important-holidays
+                    cal-china-x-general-holidays
+                    other-holidays))
+
+      )))
