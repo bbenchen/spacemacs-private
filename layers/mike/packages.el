8f563e055a66e4b66ec0de5a6ffaea1d3295a734
Mike Chen <517926804@qq.com>
2018-10-25 17:46:55 +0800

    优化ranger
    删除初始化cal-china-x时，未找到other-holidays变量

--- layers/mike/packages.el
+++ layers/mike/packages.el
@@ -13,9 +13,50 @@
 
 (defconst mike-packages
   '(
+    ranger
     fcitx
     cal-china-x
     ))
+
+;; 优化ranger
+(defun mike/post-init-ranger ()
+  ;; https://emacs-china.org/t/ranger-golden-ratio/964/2
+  (progn
+    (setq ranger-preview-file t)
+
+    (setq ranger-show-literal t)
+
+    (setq ranger-width-preview 0.55)
+
+    (setq ranger-max-preview-size 1)
+
+    (defun my-ranger ()
+      (interactive)
+      (if golden-ratio-mode
+          (progn
+            (golden-ratio-mode -1)
+            (ranger)
+            (setq golden-ratio-previous-enable t))
+        (progn
+          (ranger)
+          (setq golden-ratio-previous-enable nil))))
+
+    (defun my-quit-ranger ()
+      (interactive)
+      (if golden-ratio-previous-enable
+          (progn
+            (ranger-close)
+            (golden-ratio-mode 1))
+        (ranger-close)))
+
+    (with-eval-after-load 'ranger
+      (progn
+        ;; 修复在非golden-ratio-mode时，调用SPC-j-d或SPC-j-D时，无法按q退出ranger的BUG
+        (setq golden-ratio-previous-enable nil)
+        (define-key ranger-normal-mode-map (kbd "q") 'my-quit-ranger)))
+
+    ;; 绑定ranger快捷键
+    (spacemacs/set-leader-keys "ar" 'my-ranger)))
 
 ;; fcitx优化
 (defun mike/post-init-fcitx ()
@@ -42,7 +83,6 @@
                                            ))
       (setq calendar-holidays
             (append cal-china-x-important-holidays
-                    cal-china-x-general-holidays
-                    other-holidays))
+                    cal-china-x-general-holidays))
 
       )))
